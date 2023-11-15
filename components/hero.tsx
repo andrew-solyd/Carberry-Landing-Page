efc61752525a55fddea6e97ac809eeeaeb27bac5
ShadowPuppet66 <andrew@solydaria.co>
2023-11-15 14:21:35 -0500

    responsive tweaks

--- components/hero.tsx
+++ components/hero.tsx
@@ -5,10 +5,10 @@ const Hero = () => {
   return (
 
     <div className="flex flex-col sm:flex-row justify-center sm:justify-between items-start">          
-      <div className="rounded-lg overflow-hidden m-5">
+      <div className="flex flex-col justify-center rounded-lg overflow-hidden m-5">
         <Image src="/hero.png" alt="Photo of woman in supermarket with concerned face" width={350} height={350} />
       </div>
-      <div className="font-mono w-86 sm:w-96 pl-6 sm:pl-12 pt-2 sm:pt-6">
+      <div className="flex flex-col justify-center font-mono w-80 sm:w-96 pl-6 sm:pl-6 md:pl-12 pt-2 sm:pt-6">
         <TypedText />
       </div>
     </div>
--- components/typed-text.tsx
+++ components/typed-text.tsx
@@ -5,7 +5,7 @@ import TypeIt from "typeit-react"
 const TypedText = () => {
   return (
 
-    <div style={{ fontSize: '18pt', lineHeight: '26pt' }}>
+    <div className="text-sm sm:text-lg lg:text-2xl">
       <TypeIt       
         getBeforeInit={(instance) => {
           instance.type("I am a mother. We are a family of 4, living in Brooklyn, two small children. We need to eat healthy, all organic, no processed foods. Money is tight").pause(1000).delete(14).pause(750).type("We're on a budget.").options({ 
