5712c0563ff7c021f37f0e546c28140ffe5c92e6
ShadowPuppet66 <andrew@solydaria.co>
2023-11-27 16:32:30 -0500

    more items

--- components/product-card.tsx
+++ components/product-card.tsx
@@ -10,11 +10,11 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ image, item, quantity, cost }) => {
   return (
-    <div className="flex flex-row justify-between w-full my-1 text-sm border-b border-dashed border-gray-400">
-      <Image src={image} alt={item} width={64} height={64} className="object-cover rounded-lg" />
-      <div className="flex flex-col w-[225px] mt-1 ml-3 justify-start space-y-1">
+    <div className="flex flex-row justify-between w-full my-1 text-sm border-b border-dashed border-gray-400 pb-1">
+      <Image src={image} alt={item} width={50} height={50} className="object-cover rounded-lg" />
+      <div className="flex flex-col w-[270px] justify-start space-y-1">
         <div className="flex w-full">{item}</div>
-        <div className="flex justify-between w-full space-x-3">
+        <div className="flex justify-between w-full space-x-1">
           <div>{quantity}</div>
           <div>{cost}</div>
         </div>
--- components/table-v2.tsx
+++ components/table-v2.tsx
@@ -11,8 +11,8 @@ const CartTable: React.FC<CartTableProps> = ({ tableType }) => {
     { image: '/milk.jpg', item: 'Organic Valley 2% Milk', quantity: '1 gal', cost: '$6.29' },
     { image: '/eggs.jpg', item: '365 Organic Lg Brown Eggs', quantity: '12 ct', cost: '$4.99' },
     { image: '/milk.jpg', item: 'Daves Killer Organic 21 Grain Bread', quantity: '22 oz', cost: '$6.22' },
-    { image: '/milk.jpg', item: 'Organic Boneless Skinless Chicken Thighs', quantity: '3 lbs', cost: '$25.47' },
-    { image: '/milk.jpg', item: 'Organic Boneless Chuck Roast', quantity: '2 lbs', cost: '$27.98' },
+    { image: '/milk.jpg', item: 'Organic Bnlss Sknlss Chicken Thighs', quantity: '3 lbs', cost: '$25.47' },
+    { image: '/milk.jpg', item: 'Organic Bnlss Chuck Roast', quantity: '2 lbs', cost: '$27.98' },
     { image: '/milk.jpg', item: 'Fresh Organic Blueberries', quantity: '24 oz', cost: '$13.98' },
     { image: '/milk.jpg', item: 'Frozen Organic Strawberries', quantity: '16 oz', cost: '$6.38' },
     { image: '/milk.jpg', item: 'Organic Honeycrisp Apples', quantity: '3 lbs', cost: '$11.97' },
@@ -25,19 +25,16 @@ const CartTable: React.FC<CartTableProps> = ({ tableType }) => {
   console.log(tableType)
 
   return (
-    <div className="w-[303px] flex flex-col bg-white border border-[rgb(0,0,23)] rounded-lg py-2 px-2">
-      <ProductCard 
-        image={regularShopItems[0].image} 
-        item={regularShopItems[0].item} 
-        quantity={regularShopItems[0].quantity} 
-        cost={regularShopItems[0].cost} 
-      />
-      <ProductCard 
-        image={regularShopItems[1].image} 
-        item={regularShopItems[1].item} 
-        quantity={regularShopItems[1].quantity} 
-        cost={regularShopItems[1].cost} 
-      />
+    <div className="w-[350px] flex flex-col bg-white border border-[rgb(0,0,23)] rounded-lg py-2 px-2">
+      {regularShopItems.map((item, index) => (
+        <ProductCard 
+          key={index}
+          image={item.image} 
+          item={item.item} 
+          quantity={item.quantity} 
+          cost={item.cost} 
+        />
+      ))}
     </div>
   )
 };
