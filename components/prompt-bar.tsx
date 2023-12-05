99cc8dbefbd5f4b648b4c080c3d57e074fd2bc1b
ShadowPuppet66 <andrew@solydaria.co>
2023-12-05 12:23:43 -0500

    tuning up

--- components/prompt-bar.tsx
+++ components/prompt-bar.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react'
 import { FiCornerDownLeft } from 'react-icons/fi'
-import { FiArrowUpRight, FiX } from 'react-icons/fi'
+import { FiArrowUpRight } from 'react-icons/fi'
 
 import {
   Modal, 
@@ -62,12 +62,27 @@ const PromptBar = () => {
           ))}
         </div>
       </div>
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="center">
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="center" className="mx-5 mb-20 bg-black text-white">
         <ModalContent>
           {(onClose) => (
             <>
-              <div className="w-[350px] h-[350px] flex flex-col p-10">
-                Hello
+              <div className="flex flex-col px-5 py-10 items-center">
+                <h1 className="text-lg mb-2">Coming soon</h1>
+                <p className="text-sm text-center px-5">We're working on this feature. Join our waitlist to be the first to know when it's ready!</p>
+                <div className="flex flex-row">
+                  <form onSubmit={handleEmailSubmit} className="w-[200px] flex flex-col items-center justify-center mt-4 text-sm">
+                    <input
+                      type="email"
+                      placeholder="Your email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      className="mt-3 w-full bg-[rgb(0,0,23)] border border-white rounded-lg px-2 py-1"
+                    />
+                    <button type="submit" className="w-[150px] mt-4 bg-[rgb(156,163,175)] text-[rgb(0,0,23)] rounded-lg p-1">
+                      Submit
+                    </button>
+                  </form>
+                </div> 
               </div>
             </>
           )}
