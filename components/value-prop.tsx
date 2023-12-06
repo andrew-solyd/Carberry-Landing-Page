c167fa98d537e8fad0a0499cf331554d2c803eee
ShadowPuppet66 <andrew@solydaria.co>
2023-12-06 16:30:35 -0500

    formatting

--- components/value-prop.tsx
+++ components/value-prop.tsx
@@ -5,20 +5,20 @@ import { MdChecklistRtl } from "react-icons/md"
 const ValueProp = () => {
   return (
     <div className="mx-5 flex flex-col items-center ">
-      <div className="w-[650px] sm:w-[700px]">
+      <div className="max-w-[650px] sm:w-[700px]">
         <h1 className="text-3xl text-center mb-2">
           How it works
         </h1>
         <div className="flex flex-col items-center sm:items-start sm:flex-row">
-          <div className="mt-4 text-center mx-2 sm:mx-10 max-w-[220px] mb-3 sm:mb-0">
+          <div className="mt-4 text-center mx-2 sm:mx-6 max-w-[220px] mb-3 sm:mb-0">
             <div className="flex justify-center mb-2"><MdOutlineQuestionAnswer size={40} /></div>
             Link your loyalty account and let our AI do its magic
           </div>
-          <div className="mt-4 text-center mx-2 sm:mx-10 max-w-[220px] mb-3 sm:mb-0">
+          <div className="mt-4 text-center mx-2 sm:mx-6 max-w-[220px] mb-3 sm:mb-0">
             <div className="flex justify-center mb-2"><MdLightbulbOutline size={40} /></div>
             {"Quckly plan out what you're eating for the week"}
           </div>
-          <div className="mt-4 text-center mx-2 sm:mx-10 max-w-[220px] mb-3 sm:mb-0">
+          <div className="mt-4 text-center mx-2 sm:mx-6 max-w-[220px] mb-3 sm:mb-0">
             <div className="flex justify-center mb-2"><MdChecklistRtl size={40} /></div>
             Get full shopping list with actual store prices
           </div>
