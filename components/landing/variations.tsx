e54b55d6aeafdfd055feb30548d2e1b6fe183351
ShadowPuppet66 <andrew@solydaria.co>
2024-01-06 07:19:42 -0500

    copy

--- components/landing/variations.tsx
+++ components/landing/variations.tsx
@@ -13,8 +13,8 @@ interface VariationResult {
 }
 
 const getVariations = ({ utm }: VariationProps): VariationResult  => {
-  let header = `You've been looking for this`
-  let subheader = [`Get your perfect shopping cart in seconds. Save at least 20% every time you check out. `, `Cartberry — your AI co-pilot for savvy grocery shopping.`]
+  let header = `Meaningfully reduce your grocery expenses`
+  let subheader = [`Get your perfect shopping cart in seconds. Save at least 20% every time you check out. `, `Introducing Cartberry, your AI co-pilot for savvy grocery shopping.`]
   let image = `/hero.png`
   let cta = `Be our early adopter`
   let props = [`Link your store loyalty account and let the magic begin`, `Our AI scans all prices, sales and coupons available`, `You get a personal shopping list with max store savings`]
@@ -34,8 +34,11 @@ export default getVariations
 
 
  /*
+  header = `You've been looking for this`
+  let header = `Dramatically increase money saved on groceries`
   const header_v1 = "Feeding a family shouldn't be this expensive"
   const header_v2 = "Stop overpaying for groceries"
   const subheader_v1 = "Save time and money on every shop — Cartberry finds you the best deals for healthy, organic foods at your go-to stores."
   const subheader_v2 = "Find the best organic deals in seconds. Get instant cash savings every time you shop. Cartberry — your key to affordable, healthy eating."
+  subheader = [`Get your perfect shopping cart in seconds. Save at least 20% every time you check out. `, `Cartberry — your AI co-pilot for savvy grocery shopping.`]
   */
