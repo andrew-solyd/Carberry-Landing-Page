9cbf1216a6150de7b9ac667115734eccda00ef66
ShadowPuppet66 <andrew@solydaria.co>
2024-01-09 13:36:14 -0500

    icons

--- components/landing/value-prop.tsx
+++ components/landing/value-prop.tsx
@@ -1,5 +1,6 @@
-import { MdOutlineAccountTree, MdChecklistRtl } from "react-icons/md"
-import { AiOutlineFileSearch } from "react-icons/ai"
+import { MdOutlineEmojiNature } from "react-icons/md"
+import { TbShoppingBagSearch } from "react-icons/tb"
+import { RiFileList3Line } from "react-icons/ri"
 
 import getVariations from "@/components/landing/variations"
 
@@ -19,15 +20,15 @@ const ValueProp: React.FC<UTMProps> = ({ utm }) => {
         </h1>
         <div className="flex flex-col sm:flex-row items-center">
           <div className="text-center mx-2 sm:mx-10 max-w-[240px] mb-10 sm:mb-0">
-            <div className="flex justify-center mb-5"><MdOutlineAccountTree size={50} /></div>
+            <div className="flex justify-center mb-5"><MdOutlineEmojiNature size={50} /></div>
             {props[0]}
           </div>
           <div className="text-center mx-2 sm:mx-10 max-w-[240px] mb-10 sm:mb-0">
-            <div className="flex justify-center mb-5"><AiOutlineFileSearch size={50} /></div>
+            <div className="flex justify-center mb-5"><TbShoppingBagSearch size={50} /></div>
             {props[1]}
           </div>
           <div className="text-center mx-2 sm:mx-10 max-w-[240px] mb-6 sm:mb-0">
-            <div className="flex justify-center mb-5"><MdChecklistRtl size={50} /></div>
+            <div className="flex justify-center mb-5"><RiFileList3Line size={50} /></div>
             {props[2]}
           </div>
         </div>
