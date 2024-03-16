cb3f28824aec020481fd3d8c18662c7906d38864
ShadowPuppet66 <andrew@solydaria.co>
2024-03-15 23:58:22 -0400

    working meal cards

---
+++ components/meals/meal-ticket.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import Image from 'next/image'
+
+interface MealTicketProps {
+  activeIndex: number;
+  index: number;
+  isIncoming: boolean;
+  isOutgoing: boolean;
+}
+
+const MealTicket: React.FC<MealTicketProps> = ({ activeIndex, index, isIncoming, isOutgoing }) => {
+	let parallaxY = "-10%";
+	if (isIncoming || isOutgoing) {
+		parallaxY = "10%";
+	}
+  if (index === activeIndex) {
+    parallaxY = "10%";
+  }
+
+  return (
+    <div style={container} className="w-[275px] mb-8">
+      <div 
+				className="overflow-hidden w-[275px] h-[275px] relative"	
+				style={{ borderRadius: '1.5rem 1.5rem 0 0' }}
+			>
+				<div 
+					className="w-[470px] h-[470px] absolute"
+					style={{ top: '-50px', left: '-100px' }} 
+					data-swiper-parallax-x="-20%"
+          data-swiper-parallax-y={parallaxY}
+				>
+					<Image
+						src="/meals/meal_photo_1.webp"
+						alt="Prepared meal"
+						fill					
+						object-fit="cover"						
+					/>
+				</div>
+      </div>
+      <div className="p-4 mb-2">
+        <h2 className="text-lg font-semibold leading-snug mb-1">Chicken & Chickpea Curry with Garlic Butter Naan</h2>
+        <p className="text-xs text-gray-500 mb-3">Total cook time: 25 minutes</p>
+        <p className="text-gray-500 text-sm">Ingredients: mixed greens, squash, pomegranate seeds, mozzarella...</p>
+        {/* Add more content as needed */}
+      </div>
+    </div>
+  );
+};
+
+const container = {
+	borderWidth: "0px",
+	borderColor: "rgba(0,173,238,0.5)",
+	borderRadius: "1.5rem",
+	background: "linear-gradient(#fff, rgb(0 0 0 / 0.02))",
+	boxShadow: "0px 0px 0px 1px var(--shadow-color), 0px 1px 1px -0.5px var(--shadow-color), 0px 3px 3px -1.5px var(--shadow-color), 0px 6px 6px -3px var(--shadow-color), 0px 12px 12px -6px var(--shadow-color), 0px 24px 24px -12px var(--shadow-color)",
+	"--shadow-color": "rgb(0 0 0 / 0.06)"
+};
+
+export default MealTicket;
--- pages/meals/index.tsx
+++ pages/meals/index.tsx
@@ -1,9 +1,44 @@
+import React, { useState, useRef, useEffect } from 'react';
 import Head from 'next/head'
-import '@/app/globals.css' // Global styles
+import '@/app/globals.css'
 import Header from '@/components/universal/header'
 import Footer from '@/components/universal/footer'
+import MealTicket from '@/components/meals/meal-ticket'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import SwiperCore from 'swiper';
+import { Parallax } from 'swiper/modules'
+import 'swiper/css'
 
 const MealPage: React.FC = () => {
+	const [activeIndex, setActiveIndex] = useState(0);
+	const lastActiveIndex = useRef(0);
+	const [spaceBetween, setSpaceBetween] = useState(140);
+  const [slidesOffsetBefore, setSlidesOffsetBefore] = useState(-60);
+
+	useEffect(() => {
+    const updateSpaceBetween = () => {
+      const screenWidth = window.innerWidth;
+      if (screenWidth < 520) { // Tailwind's 'sm' breakpoint
+        setSpaceBetween(200); 
+        setSlidesOffsetBefore(screenWidth/4.37*-1) 
+      } else {
+        setSpaceBetween(140);
+        setSlidesOffsetBefore(-60) 
+      }
+    };
+
+    updateSpaceBetween(); // Call on mount to set initial value
+    window.addEventListener('resize', updateSpaceBetween); // Adjust on window resize
+
+    return () => window.removeEventListener('resize', updateSpaceBetween); // Cleanup on unmount
+  }, []);
+
+	const handleSlideChange = (swiper: SwiperCore) => {
+    const currentActiveIndex = swiper.activeIndex;
+    setActiveIndex(currentActiveIndex);
+    lastActiveIndex.current = currentActiveIndex; // Update the last active index
+  };
+
   return (
     <>
       <Head>
@@ -12,9 +47,31 @@ const MealPage: React.FC = () => {
       </Head>
       <div className="flex flex-col min-h-screen justify-between">
         <Header />
-        <div className="flex-grow flex items-center justify-center">
-          <div className="w-300 h-500 bg-white shadow-md flex items-center justify-center">
-            {/* Content goes here */}
+        <div className="flex flex-row items-center justify-center w-full">
+          <div className="w-full sm:w-[460px]"> 
+            <Swiper
+              centeredSlides={true}
+              slidesPerView={2}
+              spaceBetween={spaceBetween} // Adjust this value as needed
+              initialSlide={0}
+              slidesOffsetBefore={slidesOffsetBefore}
+              slidesOffsetAfter={0}
+              parallax={true}
+							modules={[Parallax]}
+            
+              onBeforeTransitionStart={handleSlideChange}
+            >
+              {[...Array(5)].map((_, index) => (
+								<SwiperSlide key={index}>
+									<MealTicket
+										activeIndex={activeIndex}
+										index={index}
+										isIncoming={index === activeIndex} // Assuming activeIndex is the new active slide
+										isOutgoing={index === lastActiveIndex.current} // You might need to manage state to track this
+									/>
+                </SwiperSlide>
+              ))}
+            </Swiper>
           </div>
         </div>
         <Footer />
