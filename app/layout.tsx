a60bdd20ec965bcc5fdbd8679f366c4165c14b60
ShadowPuppet66 <andrew@solydaria.co>
2024-04-09 10:06:44 -0400

    pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,13 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-
-import { Analytics } from '@/components/analytics'
+import MetaPixel from "@/components/landing/meta-pixel";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Cartberry | Cook with what\'s on sale',
-  description: 'Cartberry finds the sales at your go-to stores and creates endless recipes with an organized shopping list.  Save time & money on food for the week.'
+  title: 'Cartberry | AI-powered meal planning',
+  description: 'Introducing Cartberry. Personalized, budget-friendly meals directly from your grocer\'s sales.'
 }
 
 export default function RootLayout({
@@ -20,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
-        <Analytics />
+				<MetaPixel />
       </body>
     </html>
   )
